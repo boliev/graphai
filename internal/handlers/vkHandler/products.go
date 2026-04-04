d65e3f6e78dfc2b74e672c93e9577f05e808ddc2
voffdev <voff.web@gmail.com>
2026-04-05 00:20:15 +0300

    fix products images

--- internal/handlers/vkHandler/products.go
+++ internal/handlers/vkHandler/products.go
@@ -15,25 +15,25 @@ var items = map[string]*Product{
 		ID:       1,
 		Title:    "1 кредит",
 		Price:    10,
-		PhotoURL: "https://graphai-pay.ai256.ru/credit_1.png",
+		PhotoURL: "https://graphai-pay.ai128.ru/credit_1.png",
 	},
 	"pack_5": {
 		ID:       2,
 		Title:    "5 кредитов",
 		Price:    45,
-		PhotoURL: "https://graphai-pay.ai256.ru/credit_5.png",
+		PhotoURL: "https://graphai-pay.ai128.ru/credit_5.png",
 	},
 	"pack_10": {
 		ID:       3,
 		Title:    "10 кредитов",
 		Price:    80,
-		PhotoURL: "https://graphai-pay.ai256.ru/credit_10.png",
+		PhotoURL: "https://graphai-pay.ai129.ru/credit_10.png",
 	},
 	"pack_25": {
 		ID:       4,
 		Title:    "25 кредитов",
 		Price:    200,
-		PhotoURL: "https://graphai-pay.ai256.ru/credit_25.png",
+		PhotoURL: "https://graphai-pay.ai128.ru/credit_25.png",
 	},
 }
 
